fbed572fa23fa258d7109e5bb0e73582c03488ed
codinghunter-source <rohitkumawat6367@gmail.com>
2026-05-26 01:45:24 +0530

    fixed render moblie support

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,4 +1,6 @@
-const socket=io();
+const socket = io(window.location.origin, {
+    transports: ["websocket", "polling"]
+});
 const chess=new Chess();
 const boardElement =document.querySelector(".chessboard");
 
